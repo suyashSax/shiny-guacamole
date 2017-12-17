96bbed926eba27e17d9e5a53ebccd96ea3a1cf3e
suyashSax <suyash2099@gmail.com>
2017-12-17 13:43:25 +0400

    Schedule daily db update

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 const axios = require('axios')
 const express = require('express')
 const bodyParser = require('body-parser')
+const moment = require('moment')
+const schedule = require('node-schedule')
 
 const keys = require('./config/keys')
 require('./model')
@@ -24,6 +26,47 @@ app.get('/btc/:date', (req, res) => {
     })
 })
 
+/*
+GET: ALL BTC HISTORY
+This should be refactored to return an object of records key'd by date...
+*/
+
+app.get('/btc/', (req, res) => {
+    Price.find({}).then((objects) => {
+        res.send(objects)
+    }, (err) => {
+        res.status(400).send(err)
+    })
+})
+
+/*
+UPDATE DATA STORE EVERY MIDNIGHT
+*/
+var today = moment().subtract(1, "days").format('YYYY-MM-DD')
+
+schedule.scheduleJob('0 0 * * *', () => {
+    axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=' + '2017-12-16' + '&end=' + today)
+        .then((result) => {
+            var obj = result.data.bpi
+            Object.entries(obj).forEach(([key, val]) => {
+                Price.findOne({date: key}).then((existingKey) => {
+                    if (existingKey){
+                        // Do nothing, key exists
+                    }
+                    else {
+                        new Price({
+                            date: key,
+                            btc: val
+                        }).save()
+                    }
+                })
+            })
+        })
+        .catch((err) => {
+            console.log("Error")
+    })
+})
+
 /* POPULATE DATABASE
 
 axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=' + '2010-07-17' + '&end=' + '2017-12-16')
@@ -32,7 +75,7 @@ axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=' + '2010
         Object.entries(obj).forEach(([key, val]) => {
             Price.findOne({date: key}).then((existingKey) => {
                 if (existingKey){
-
+                    // Do nothing, key exists
                 }
                 else {
                     new Price({
