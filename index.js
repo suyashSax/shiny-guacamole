0fbfded2c1cf35155584040cbeca8365639b5b88
suyashSax <suyash2099@gmail.com>
2017-12-17 12:40:52 +0400

    GET: BTC Price

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const express = require('express')
 const mongoose = require('mongoose')
-const keys = require('./config/keys')
 const axios = require('axios')
+const express = require('express')
+const bodyParser = require('body-parser')
 
+const keys = require('./config/keys')
 require('./model')
 const Price = mongoose.model('Prices')
 
 mongoose.connect(keys.mongoURI)
 const app = express()
 
+/*
+GET: BTC PRICE
+INPUT: YYYY-MM-DD
+*/
+
+app.get('/btc/:date', (req, res) => {
+    var date = req.params.date
+    Price.findOne({date: date}).then((obj) => {
+        res.send(obj)
+    }, (err) => {
+        res.status(400).send(err)
+    })
+})
 
 /* POPULATE DATABASE
 
@@ -36,5 +50,5 @@ axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=' + '2010
 */
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT)
-console.log("Started server on port", PORT)
+app.listen(PORT, () => console.log("Started server on port", PORT))
+module.exports = {app}
